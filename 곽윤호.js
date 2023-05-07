703dd00f25e030e3b0ef1eb4581bceacabee5130
KwakYounHo <kwakyh951@gmail.com>
2023-05-07 21:16:22 +0900

    문제 분석하기

---
+++ 곽윤호.js
@@ -0,0 +1,36 @@
+function exampleOne(numberValue) {
+  // sequence 1
+  function integerCheck(data) {
+    if (Number.isInteger(data) === true) {
+      return data;
+    } else {
+      return new Error('이 함수의 매개변수는 정수타입이어야 합니다.')
+    }
+  }
+  // sequence 2
+  function innerOne(one) {
+    return one + 1;
+  }
+  // sequence 3
+  function innerTwo(two) {
+    return two + 2;
+  }
+  // sequence 4
+  function innerThree(three) {
+    return three + 3;
+  }
+  // sequence 5
+  function innerFour(four) {
+    return four + 4;
+  }
+
+  integerCheck(numberValue)
+  const first = innerOne(numberValue);
+  const second = innerTwo(first);
+  const third = innerThree(second);
+  const fourth = innerFour(third);
+  return fourth
+}
+
+const result = exampleOne(10);
+console.log(result);
