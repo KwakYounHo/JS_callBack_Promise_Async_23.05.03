2075d44e5de2549758156c332b4be344d250aaa1
KwakYounHo <kwakyh951@gmail.com>
2023-05-07 21:38:53 +0900

    Q2 답안 작성

--- 곽윤호.js
+++ 곽윤호.js
@@ -1,33 +1,29 @@
-//? Q1 =============================================================================
-function exampleOne(numberValue) {
-  // sequence 1
-  function integerCheck(data) {
-    if (Number.isInteger(data) === true) {
-      return data;
+//? Q2 =============================================================================
+function exampleOne(data, callback) {
+  let result;
+  new Promise((resolve, reject)=>{
+    if (Number.isInteger(data)===true) {
+      return resolve(data);
     } else {
-      return new Error('이 함수의 매개변수는 정수타입이어야 합니다.')
+      reject('매개변수는 정수타입이어야 합니다.');
     }
-  }
-  // sequence 2
-  function innerOne(one) {
-    return one + 1;
-  }
-  // sequence 3
-  function innerTwo(two) {
-    return two + 2;
-  }
-  // sequence 4
-  function innerThree(three) {
-    return three + 3;
-  }
-  // sequence 5
-  function innerFour(four) {
-    return four + 4;
-  }
-  
-  return innerFour(innerThree(innerTwo(innerOne(integerCheck(numberValue)))))
+  })
+    .then(data => {
+      return data + 1;
+    })
+    .then(data => {
+      return data + 2;
+    })
+    .then(data => {
+      return data + 3;
+    })
+    .then(data => {
+      return data + 4;
+    })
+    .then(data => {
+      callback(data)
+    })
 }
 
-const result = exampleOne(10);
-console.log(result);
-//? Q1 =============================================================================
+exampleOne(10,data=>console.log(data));
+//? Q2 =============================================================================
