dae5c82b31f0ad3bbb2b8ab92db926d1c464d2f1
KwakYounHo <kwakyh951@gmail.com>
2023-05-07 22:45:19 +0900

    Q4 답안 작성

--- 곽윤호.js
+++ 곽윤호.js
@@ -1,41 +1,39 @@
-function exampleOne(numberValue) {
-  // sequence 1
-  function integerCheck(data) {
-    if (Number.isInteger(data) === true) {
-      return data;
+//? Q4 =========================================================================
+class exampleOne {
+  constructor(NumberValue) {
+    this._data = NumberValue;
+  }
+  set _data(value) {
+    if (Number.isInteger(value)===true) {
+      return this.data = value;
     } else {
-      return new Error('이 함수의 매개변수는 정수타입이어야 합니다.')
+      throw new Error('매개변수는 정수타입이어야 합니다.');
     }
   }
-  // sequence 2
-  function innerOne(one) {
-    return one + 1;
-  }
-  // sequence 3
-  function innerTwo(two) {
-    return two + 2;
-  }
-  // sequence 4
-  function innerThree(three) {
-    return three + 3;
-  }
-  // sequence 5
-  function innerFour(four) {
-    return four + 4;
-  }
-
-  integerCheck(numberValue)
-  const first = innerOne(numberValue);
-  const second = innerTwo(first);
-  const third = innerThree(second);
-  const fourth = innerFour(third);
-  return fourth
+}
+exampleOne.prototype.innerOne      = function() {return this.data + 1;};
+exampleOne.prototype.innerTwo      = function() {return this.data + 2;};
+exampleOne.prototype.innerThree    = function() {return this.data + 3;};
+exampleOne.prototype.innerFour     = function() {return this.data + 4;};
+exampleOne.prototype.functionBout1 = function() {
+  let result;
+  result = myClass.innerOne();
+  result += 2;
+  result += 3;
+  result += 4;
+  return result;
+}
+exampleOne.prototype.functionBout2 = function() {
+  let result;
+  result =  myClass.innerOne();
+  result += myClass.innerTwo();
+  result += myClass.innerThree();
+  result += myClass.innerFour();
+  return result
 }
 
-const result = exampleOne(10);
-console.log(result);
-
-// Q1. 콜백 지옥으로 처리하기
-// Q2. Promise로 처리하기
-// Q3. async, await로 처리하기
-// Q4. 부모 함수를 클래스로 만들고, 인스턴스에 '동적'으로 4개의 inner함수를 메서드로 추가해보기
+const myClass = new exampleOne(10);
+console.log(myClass);
+console.log(myClass.functionBout1());
+console.log(myClass.functionBout2());
+//? Q4 =========================================================================
